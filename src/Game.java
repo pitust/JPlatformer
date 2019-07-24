e71e302f83ac49888df007bcce56cd9ba6185af6
booky10 <booky10@users.noreply.github.com>
2019-07-24 10:35:58 +0200

    2 more levels added

--- src/Game.java
+++ src/Game.java
@@ -38,7 +38,7 @@ public class Game {
                 "portal5.png" });
         // Auto-gen, use <Z>export
 
-        Level.ELEVATORTEST.setLevel();
+        Level.JUMPNRUNTEST2.setLevel();
 
         p = new Player(app, level);
         p.init();
