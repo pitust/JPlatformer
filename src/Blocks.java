64ee8e2ab53bc14053b7c5df2fb5ac36f6e42612
booky10 <booky10@users.noreply.github.com>
2019-07-24 10:45:50 +0200

    Enum "Blocks" added

--- src/Blocks.java
+++ src/Blocks.java
@@ -1,13 +1,46 @@
 public enum Blocks {
 
     DIRT,
-    GRASS,
-    WATER;
+    GOALFLAG,
+    PORTAL,
+    SPIKE,
+    WIFI,
+    AIR;
 
-    public String getFileName() {
+    public String getName() {
         switch (this) {
+            case AIR:
+                return "air.png";
+            case WIFI:
+                break;
+            case PORTAL:
+                break;
+            case SPIKE:
+                break;
+            case GOALFLAG:
+                break;
+            default:
+                return null;
         }
         return null;
+    }
+
+
+    public String getNameForDirt(boolean isBottom, boolean isLeft, boolean isRight, boolean isTop) {
+        String dirt = "dirt";
+        if (isTop) {
+            dirt = "grass";
+        }
+        if (isLeft && !isRight) {
+            dirt += "L";
+        }
+        if (isRight && !isLeft) {
+            dirt += "R";
+        }
+        if (isBottom) {
+            dirt += "B";
+        }
+        return dirt;
     }
 
 }
