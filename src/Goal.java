9284947738c8f18e8b389802d008ef6e59357ba9
Darkhell66659 <Darkhell66659@users.noreply.github.com>
2019-07-26 10:08:00 +0200

    Added music (that doesn't work though) and a test of what happens if you win

--- src/Goal.java
+++ src/Goal.java
@@ -14,4 +14,15 @@ public class Goal {
     {
         app.image(app.loadImage("targetFlag.png"), Util.globX(posX), Util.globY(posY), 50, 50);
     }
+    public boolean check_goal(int PposX, int PposY)
+    {
+        if(PposX == posX && PposY == posY)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
